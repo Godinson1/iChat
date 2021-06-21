012a71834e5f5cbbae5398aca5010db7e1c8579a
Godinson1 <josephgodwin274@yahoo.com>
2021-06-21 14:27:58 +0100

    test-prod

--- base/utililties/midleware/middleware.ts
+++ base/utililties/midleware/middleware.ts
@@ -26,7 +26,9 @@ export const auth = async (ctx: ExpressContext) => {
   if (token) {
     jwt.verify(token, `${process.env.JWT_SECRET}`, (err, decodedToken) => {
       //Set the authenticated user in user context
+      if (err) console.log(err);
       console.log(decodedToken);
+      console.log("secret", process.env.JWT_SECRET);
       ctx.user = decodedToken as IDecodedToken;
     });
     console.log("user", ctx.user);
