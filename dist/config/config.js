e081c33a41b9cc00c86506b3460c0bf1613fe477
Godinson1 <josephgodwin274@yahoo.com>
2023-03-17 14:58:41 +0100

    fix config file

---
+++ dist/config/config.js
@@ -0,0 +1,28 @@
+"use strict";
+require("dotenv").config();
+module.exports = {
+    development: {
+        username: process.env.PG_DEV_USER,
+        password: process.env.PG_DEV_PASSWORD,
+        database: process.env.PG_DEV_DB,
+        host: process.env.PG_DEV_HOST,
+        dialect: process.env.PG_DIALET,
+        url: process.env.PG_URL,
+        logging: false,
+    },
+    test: {
+        username: process.env.PG_DEV_USER,
+        password: process.env.PG_DEV_PASSWORD,
+        database: process.env.PG_DEV_DB,
+        host: process.env.PG_DEV_HOST,
+        dialect: process.env.PG_DIALET,
+    },
+    production: {
+        username: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DB,
+        host: process.env.PG_HOST,
+        url: process.env.PG_URL,
+        dialect: "postgres",
+    },
+};
